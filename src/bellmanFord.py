4165e3ab3cd92ab2cecc86327056dd3219aa7af7
Marcoshsc <marcoshscunha@hotmail.com>
2020-09-24 13:51:10 -0300

    Create Bellman-Ford algorithm

---
+++ src/bellmanFord.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+def bellmanFordAlgorithm(graph: List[List[float]], origin: int) -> Tuple[List[float], List[int]]:
+
+    dist: List[float] = [float('inf') for v in graph]
+    pred: List[int] = [None for v in graph]
+    dist[origin] = 0
+
+    edges: List[Tuple[int, int]] = []
+    indexes = range(len(graph))
+    for i in indexes:
+        for j in indexes:
+            if graph[i][j] != 0:
+                edges.append((i, j))
+    
+    for _ in indexes:
+        trocou = False
+        for edge in edges:
+            u, v = edge
+            if dist[v] > dist[u] + graph[u][v]:
+                dist[v] = dist[u] + graph[u][v]
+                pred[v] = u
+                trocou = True
+        if not trocou:
+            break
+    
+    return dist, pred
