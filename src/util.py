9c0b31fe16475dd527984888d0a392797de8e8e5
Marcoshsc <marcoshscunha@hotmail.com>
2020-09-24 15:26:01 -0300

    cleanup getPath function

--- src/util.py
+++ src/util.py
@@ -2,12 +2,13 @@ from typing import List
 
 def getPath(pred: List[int], source: int, target: int) -> List[int]:
     path: List[int] = [target]
-    cost: float = 0
     aux = target
     while aux != source:
         aux = pred[aux]
         path.insert(0, aux)
-    return pathdef getPathFromMatrix(pred: List[List[int]], source: int, target: int):
+    return path
+
+def getPathFromMatrix(pred: List[List[int]], source: int, target: int):
     path: List[int] = [target]
     aux = target
     while aux != source:
