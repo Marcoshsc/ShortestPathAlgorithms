be26b620081da1d09ee019d25189440b6659edf2
Marcoshsc <marcoshscunha@hotmail.com>
2020-09-24 13:24:54 -0300

    create util function to retrieve path from predecessors

---
+++ src/util.py
@@ -0,0 +1,10 @@
+from typing import List
+
+def getPath(pred: List[int], source: int, target: int):
+    path: List[int] = [target]
+    cost: float = 0
+    aux = target
+    while aux != source:
+        aux = pred[aux]
+        path.insert(0, aux)
+    return path
