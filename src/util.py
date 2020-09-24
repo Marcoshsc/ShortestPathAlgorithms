428a302a5b8996a2c619e89125bb9053948216f6
Marcoshsc <marcoshscunha@hotmail.com>
2020-09-24 15:24:55 -0300

    added function to get path and cost from pred/dist matrixes

--- src/util.py
+++ src/util.py
@@ -7,4 +7,16 @@ def getPath(pred: List[int], source: int, target: int) -> List[int]:
     while aux != source:
         aux = pred[aux]
         path.insert(0, aux)
+    return pathdef getPathFromMatrix(pred: List[List[int]], source: int, target: int):
+    path: List[int] = [target]
+    aux = target
+    while aux != source:
+        aux = pred[source][aux]
+        path.insert(0, aux)
     return path
+
+def getCost(dist: List[List[float]], path: List[int]):
+    cost = 0
+    for n in range(len(path) - 1):
+        cost += dist[path[n]][path[n + 1]]
+    return cost
