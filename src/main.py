eebae29a8c7d00c581644a4cdc746e4368fd1de8
Marcoshsc <marcoshscunha@hotmail.com>
2020-10-01 14:51:11 -0300

    added CLI to execute the algorithms

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import argparse
+from runner import runAlgorithms
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-p', '--predef',  action='store_true', help='Utilizar configurações pré-definidas na pasta resources')
+parser.add_argument('-s', '--save', action='store_true', help='Salvar o grafo gerado')
+parser.add_argument('-ng', '--nogenerate', action='store_true', help='Utilizar grafo provido pelo usuário')
+
+args = parser.parse_args()
+
+runAlgorithms(not args.nogenerate, args.predef, args.save)
