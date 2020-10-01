4e12c6cf1cc8d42e280d86448153bb1a8d89c0e0
Marcoshsc <marcoshscunha@hotmail.com>
2020-09-30 22:40:35 -0300

    split bellman ford into two algorithms, one normal and another efficient

--- src/bellmanFord.py
+++ src/bellmanFord.py
@@ -14,6 +14,28 @@ def bellmanFordAlgorithm(graph: List[List[float]], origin: int) -> Tuple[List[fl
                 edges.append((i, j))
     
     for _ in indexes:
+        for edge in edges:
+            u, v = edge
+            if dist[v] > dist[u] + graph[u][v]:
+                dist[v] = dist[u] + graph[u][v]
+                pred[v] = u
+    
+    return (dist, pred)
+
+def bellmanFordAlgorithmEficient(graph: List[List[float]], origin: int) -> Tuple[List[float], List[int]]:
+
+    dist: List[float] = [float('inf') for v in graph]
+    pred: List[int] = [None for v in graph]
+    dist[origin] = 0
+
+    edges: List[Tuple[int, int]] = []
+    indexes = range(len(graph))
+    for i in indexes:
+        for j in indexes:
+            if graph[i][j] != 0:
+                edges.append((i, j))
+    
+    for _ in indexes:
         trocou = False
         for edge in edges:
             u, v = edge
